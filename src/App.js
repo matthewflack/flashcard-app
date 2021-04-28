cdd2df4e761bbc8626668789bc6a8618c7a5f867
Matthew Flack <maq.flack@gmail.com>
2021-04-28 15:34:18 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [flashcards, setFlashcards = useState(SAMPLE_FLASHCARDS))]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello World</h1>
+      {flashcards}
+    </div>  
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id:1,
+    question:'What is 2 + 2',
+    answer: '4',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id:2,
+    question:'Question 2',
+    answer: 'Answer',
+    options:[
+      'Answer',
+      'Answer 1',
+      'Answer 2',
+      '5'
+    ]
+  }
+
+]
 
 export default App;
---
+++ src/FlachcardList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import React from 'react'
+
+export default function FlachcardList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
