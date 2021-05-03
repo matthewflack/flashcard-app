c05deddb25a264194f577c4f21ebc6bdf4e33437
Matthew Flack <maq.flack@gmail.com>
2021-05-03 10:17:58 -0700

    Add a lot. Styles / Axios API Call / decodeString fn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(()=>{
+    axios.get(API_ENDPOINT)
+    .then(res=>{
+      setFlashcards(res.data.results.map((questionItem,index)=>{
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a))
+          , answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random()- .5)
+
+        }
+      }))
+    })
+  },[])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textArea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <FlashcardList flashcards={flashcards}/>
   );
 }
+
+
+const API_ENDPOINT = 'https://opentdb.com/api.php?amount=10';
 
 const SAMPLE_FLASHCARDS = [
   {
